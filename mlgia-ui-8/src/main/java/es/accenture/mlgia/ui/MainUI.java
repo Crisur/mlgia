2bab04e89181f50dd746b5329bbe4f375ac54fcf
Molina Jimena <rafael.molina.jimena@accenture.com>
2018-05-17 16:14:26 +0200

    Integration Assistant

--- mlgia-ui-8/src/main/java/es/accenture/mlgia/ui/MainUI.java
+++ mlgia-ui-8/src/main/java/es/accenture/mlgia/ui/MainUI.java
@@ -1,22 +1,28 @@
 package es.accenture.mlgia.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.github.appreciated.material.MaterialTheme;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Viewport;
+import com.vaadin.data.Binder;
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import es.accenture.mlgia.dto.MessageDTO;
 import es.accenture.mlgia.ui.audio.AudioRecorder;
+import es.accenture.mlgia.ui.consumer.ConsumerAssistant;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -27,13 +33,52 @@ public class MainUI extends UI {
 	
 	//ResponsiveLayout layout;
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	Binder<MessageDTO> binder;
+	TextField tfQuery;
+	Panel contentPanel;
+	
+	
+	
+	
+	@Autowired
+	ConsumerAssistant consumerAssistant;
+	
 	@Override
 	protected void init(VaadinRequest request) {
-		// TODO Auto-generated method stub
-
 		buildUI();
+		log.debug("Compoentes UI Ok");
+		
+		initBinder();
+		initConversation();
 
 	}
+
+	private void initBinder() {
+		binder = new Binder<>(MessageDTO.class);
+		binder.setBean(MessageDTO.builder().build());
+		
+		binder.forField(tfQuery)
+			.bind(MessageDTO::getMessageIn, MessageDTO::setMessageIn);
+		
+	}
+
+	private void initConversation() {
+		
+		MessageDTO messageDTO = consumerAssistant.initAssistant();
+		binder.getBean().setConversationId(messageDTO.getConversationId());
+	}
+	
+	private void clickSendText(ClickEvent event) {
+		consumerAssistant.invokeAssistant(binder.getBean());
+		
+	}
+	
 
 	private void buildUI() {
 		// TODO Auto-generated method stub
@@ -43,29 +88,29 @@ public class MainUI extends UI {
 		lbinfo.addStyleName(MaterialTheme.LABEL_BOLD);
 		
 		
+		// Chat Area
+		
+		
+		
 		// Bottom
 		// -----------------------------
 		Button btMicro = new Button(VaadinIcons.MICROPHONE);
 		btMicro.addStyleName(MaterialTheme.BUTTON_FLAT + " " + MaterialTheme.BUTTON_FLOATING_ACTION);
 		btMicro.setHeight("50px");
 		btMicro.setWidth("50px");
-		btMicro.addClickListener(new ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                log.debug("Clic");
-            }
-		});
+		btMicro.addClickListener(this::clickMicro);
 		
-		TextField tfQuery = new TextField();
+		tfQuery = new TextField();
 		tfQuery.setPlaceholder("Enter your query");
 		tfQuery.setWidth("100%");
 		
 		
 		Button btSendText = new Button(VaadinIcons.PAPERPLANE);
 		btSendText.addStyleName(MaterialTheme.BUTTON_FLAT + " " + MaterialTheme.BUTTON_FLOATING_ACTION);
-		btSendText.addClickListener(e -> log.debug("click"));
+		btSendText.addClickListener(this::clickSendText);
 		btSendText.setHeight("50px");
 		btSendText.setWidth("50px");
+		btSendText.setClickShortcut(ShortcutAction.KeyCode.ENTER);
 		
 
 		HorizontalLayout hlBotom = new HorizontalLayout();
@@ -91,8 +136,13 @@ public class MainUI extends UI {
 		setContent(rootLayout);
 
 	}
+	
+	
 
-	private void clickMicro() {
+
+
+
+	private void clickMicro(ClickEvent event) {
 		// TODO Auto-generated method stub
 		final AudioRecorder recorder = new AudioRecorder();
 		 
